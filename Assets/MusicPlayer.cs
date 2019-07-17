606d798a179343a4fc0925e9a0c6c81f4784d091
adv7 <cud4xx@gmail.com>
2019-07-17 18:19:35 +0200

    adding terrain / adding music / adding ship / making intro / terrain painting

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Invoke("LoadFirstScene", levelLoadDelay);      
+    }
+
+    // Update is called once per frame
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
