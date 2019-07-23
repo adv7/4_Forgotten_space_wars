7f83ecb46a6a34979ea9e2525a584e36cb1f19c4
adv7 <cud4xx@gmail.com>
2019-07-23 19:15:06 +0200

    cleaning in project's assets

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,22 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     [SerializeField] float levelLoadDelay = 2f;
 
     AudioSource audioSource;
 
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        Invoke("LoadFirstScene", levelLoadDelay);      
+        Invoke("LoadFirstScene", levelLoadDelay);
     }
 
     // Update is called once per frame
